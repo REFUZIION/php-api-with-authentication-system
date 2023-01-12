62f29d0eaef913c0bc03ea21504aa46225cc1656
Diederik Veenstra <d.veenstra@estg.eu>
2023-01-12 11:28:38 +0100

    Renamed retrieve-data to read.php
    Signed-off-by: Diederik Veenstra <d.veenstra@estg.eu>

--- api/data/read.php
+++ api/data/read.php
@@ -1,5 +1,5 @@
 <?php // 2023 - Diederik Veenstra <diederik@refuzion.nl>
-require_once("../config.php");
+require_once("../../config.php");
 
 if(!isset($_POST['table_name'])) {
     // Invalid API key
@@ -7,9 +7,17 @@ if(!isset($_POST['table_name'])) {
     echo json_encode(array("message" => "No search criteria received."));
     exit;
 }
-// Get the API key and table name from the POST request
-$api_token = $_POST['api_token'];
-$table_name = $_POST['table_name'];
+
+// Sanitize the API key
+$api_token = filter_var($_POST['api_token'], FILTER_SANITIZE_STRING);
+
+// Sanitize the table name and check that it's a valid table name
+$table_name = filter_var($_POST['table_name'], FILTER_SANITIZE_STRING);
+if (!preg_match('/^[a-zA-Z0-9_]+$/', $table_name)) {
+    http_response_code(400);
+    echo json_encode(array("message" => "Invalid table name."));
+    exit;
+}
 
 // Check if the API key is valid
 $stmt = $db->prepare("SELECT id FROM authentication_tokens WHERE token = ?");
@@ -23,11 +31,9 @@ if ($result->num_rows === 0) {
     exit;
 }
 
-
-
 // Prepare the SELECT statement
-$stmt = $db->prepare("SELECT * FROM ? WHERE 1");
-$stmt->bind_param("s", $table_name);
+$query = "SELECT * FROM $table_name WHERE 1";
+$stmt = $db->prepare($query);
 
 // Execute the statement
 $stmt->execute();
@@ -37,6 +43,7 @@ if ($result->num_rows === 0) {
     echo json_encode(array("message" => "No results found."));
     exit;
 }
+
 // Fetch the result as an associative array
 $output = $result->fetch_all(MYSQLI_ASSOC);
 
